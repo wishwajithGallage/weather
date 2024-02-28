0a0bb96874ae30a8c5f6fdb4d5be02d5a5f32ee2
wishwajithGallage <112330401+wishwajithGallage@users.noreply.github.com>
2024-02-28 23:48:48 +0530

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.pexels.com/photos/459451/pexels-photo-459451.jpeg?auto=compress&cs=tinysrgb&w=300");
+background-image: "1.jpeg";
 background-size: 100%;
 background-position: top left;
 background-repeat: no-repeat;
