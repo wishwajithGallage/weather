7dd8457318439e4c943b9bae2d57b2703da4b93d
wishwajithGallage <112330401+wishwajithGallage@users.noreply.github.com>
2024-02-28 23:35:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+import numpy as np
+
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.pexels.com/photos/459451/pexels-photo-459451.jpeg?auto=compress&cs=tinysrgb&w=300");
+background-size: 100%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# loading the saved models
+
+weather_model = pickle.load(open('/home/dinith/Desktop/england weather data/weather_model.sav', 'rb'))
+
+
+
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('England_weather',
+                          
+                          ['England_weather_prediction_System'],
+                          icons=['activity'],
+                          default_index=0)
+    
+    
+
+        
+    
+    
+
+# Parkinson's Prediction Page
+if (selected == "England_weather_prediction_System"):
+    
+    # page title
+    st.title("England_weather_prediction_System using ML")
+    
+    col1, col2, col3, col4 = st.columns(4)  
+    
+    with col1:
+        Temperature = st.text_input('Temperature (C)')
+        
+    with col2:
+       Speed = st.text_input('Wind Speed (km/h)')
+        
+    with col3:
+       Pressure = st.text_input('Pressure (millibars)')
+        
+    with col4:
+        Humidity = st.text_input('Humidity')
+
+        
+
+        
+    
+    
+    # code for Prediction
+    England_weather = ''
+    
+    # creating a button for Prediction    
+    if st.button("England_weather_prediction_System_Test_Result"):
+        # Assuming Time, V1 to V28, and Amount are variables with proper numeric values
+        input_data = np.array([[Temperature,Speed,Pressure, Humidity]], dtype=float)
+
+        # Now use this input_data for prediction
+        England_weather_prediction_System = weather_model.predict(input_data)
+        #Credit_Card_Fraud_Detection = credit_model.predict([[Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount]])                          
+        
+        if (England_weather_prediction_System[0] == 0):
+          England_weather = "Rain"
+        else:
+         England_weather = "Snow"
+        
+    st.success(England_weather)
+
+
+
+
+
+
